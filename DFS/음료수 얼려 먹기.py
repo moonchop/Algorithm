17b77759d17b7f6e4815e151b9494c802845ea05
moonchop <minssu913@naver.com>
2023-01-10 23:32:56 +0900

    계수정렬

--- DFS/음료수 얼려 먹기.py
+++ DFS/음료수 얼려 먹기.py
@@ -5,6 +5,7 @@ n,m = map(int,sys.stdin.readline().split())
 graph=[]
 for i in range(n):
   graph.append(list(map(int,input())))
+  
 def dfs(x,y):
   if x < 0 or x >=n or y < 0 or y >= m:
     return False
