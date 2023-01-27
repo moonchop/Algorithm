5e647124ac077f36d9b5d08a83cbc4d1cf4911e5
moonchop <moonchop913@gmail.com>
2023-01-27 18:02:29 +0900

    최단거리 : 플로이드 워셜 알고리즘

---
+++ 최단거리/미래 도시.py
@@ -0,0 +1,34 @@
+import sys
+
+INF = int(1e9)
+n,m = map(int,sys.stdin.readline().split())
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+#각 간선에 대한 정보 입력받고, 1로 초기화
+for _ in range(m):
+    a ,b = map(int,sys.stdin.readline().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+    
+x,k = map(int,sys.stdin.readline().split())
+
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b],graph[a][k]+graph[k][b])
+            
+distance = graph[1][k] + graph[k][x]
+
+if distance >= INF:
+    print("-1")
+    
+else:
+    print(distance)
