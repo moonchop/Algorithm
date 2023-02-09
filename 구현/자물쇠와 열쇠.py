5808f5d4cf5e9cb03ce3090cd1e62123e9dd1480
moonchop <moonchop913@gmail.com>
2023-02-09 17:53:41 +0900

    구현 : 자물쇠와 열쇠 설계 및 절반 맞춤

--- 구현/자물쇠와 열쇠.py
+++ 구현/자물쇠와 열쇠.py
@@ -1,4 +1,12 @@
-# init
-# 1. 각 행마다 비교하는데, lock이 0일 때만 비교하고, 1이면 비교하지 않는다.
-# 2. 회전을 4번해야하기에 총 4번 반복한다.
-# 3. 만약 모든 lock이 1이면 True를 반환한다.
+## init
+# 1. m*3인 2차원 배열 생성
+# 2. 정 가운데에 lock값 넣기
+# 3. 반복문으로 회전시키면서 4번 반복
+#    1. 1 ~ m*2까지 반복(x,y 이중반복문으로)
+#       1. 열쇠넣기
+#       2. 열리는지 체크(true false 반환)
+#       -> true이면 return True
+#       -> false이면 다음 회전
+
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/60059
