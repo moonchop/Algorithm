7d1e17e747cb86a946b5894d6a31a634c99c210e
moonchop <minssu913@naver.com>
2024-01-10 23:37:01 +0900

    백준: math(1)

--- 백준/수학/소수 최소 공배수.py
+++ 백준/수학/소수 최소 공배수.py
@@ -5,6 +5,9 @@ input = sys.stdin.readline
 N = int(input())
 arr = list(map(int,input().split()))
 
+gcd=1
+mul_prime=1
+
 def is_prime(n):
   for i in range(2,int(n**(1/2)+1)):
     if n%i == 0:
@@ -14,10 +17,10 @@ def is_prime(n):
     
 primes = list(set([i for i in arr if is_prime(i)]))
 
-
-gcd = math.gcd(*primes)
-mul_prime = math.prod(primes)
-
+for i in primes:
+  gcd = math.gcd(gcd,i)
+  mul_prime *= i
+  
 
 if len(primes) > 0:
   print(mul_prime//gcd)
